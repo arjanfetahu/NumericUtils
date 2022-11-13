d739b4bce07103d17cd584921c0704a9ff54e409
Arjan Feta <arjanfetahu@gmail.com>
2022-11-13 15:14:08 +0100

    Exported NumericConvertion functions

--- src/NumericUtils.jl
+++ src/NumericUtils.jl
@@ -2,8 +2,7 @@ module NumericUtils
 
 include("NumericConvertions.jl")
 
-export bin2hex
-
-println("Hello!!")
+export bin2fract, bin2uint, bin2sint, int2unsigned, int2signed2C, int2fixed,
+       float2fixed, bin2hex, hex2bin
 
 end
